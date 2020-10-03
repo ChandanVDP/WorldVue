b86243e5002abc8704dc09051af7f4461688a61b
ChandanVDP <visualdictionaryproject@gmail.com>
2020-10-03 11:38:52 +0530

    VS Config & Prettier

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">HOME</router-link>
-      <router-link to="/about">ABOUT</router-link>
+      <router-link :to="{ name: 'event-list' }">Event List</router-link>
+      <router-link :to="{ name: 'event-show' }">Event Show</router-link>
+      <router-link :to="{ name: 'event-create' }">Event Create</router-link>
     </div>
-    <router-view></router-view>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: darkkhaki;
+  color: darkturquoise;
 }
 #nav {
   padding: 30px;
 }
 #nav a {
-  font-weight: bold;
   color: darkorchid;
 }
 #nav a.router-link-exact-active {
-  color: darkolivegreen;
+  color: blue;
 }
 </style>
